db77577e656fbfc93a446e06fd1c4c11009598f2
nathankliewer <nathandkliewer@gmail.com>
2018-11-12 21:04:23 -0800

    control flow and operators lesson

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,123 @@
-window.alert("Hello world!");
-alert("Hello world!");
+///window.alert("Hello world!");
+///alert("Hello world!");
+
+///var myName = "Kliewer";
+
+    ///alert('Hello, ' + myName + '!'); //ES5
+
+    ///alert(`Hello, ${myName}!`); //ES6
+
+// const str1 = 'you are my brother Anika';
+// const str2 = new String(str1);
+//
+// console.log(str1 == str2);
+// console.log(str1 === str2);
+//
+// console.log(0.1+0.2)
+// console.log(.999999)
+// console.log(0/0);
+//
+//
+// let number = 1;
+// console.log('number', number);
+//
+//
+// let cartTotal = 0;
+//
+// const item = 9.99;
+// const Item2 = 7.99;
+// const discount = 5.99;
+// on those
+///console.log(cartTotal += item);
+///console.log(cartTotal += Item2);
+///console.log(cartTotal -= discount);
+
+
+///let answer1 = prompt("Guess an number");
+///let answer2 = prompt("guess my number?");
+///alert(+answer1 + +answer2);
+
+///control flow
+//
+// const firstName = "Ben";
+// if (firstName === "Jacob"){
+//
+// console.log(Your name is Jacob);
+//
+//
+//
+//
+//   //if statment evluates true this run
+//   // Indent within curly braces
+//
+//   console.log('uzzah! This is true.')
+// }
+// else {
+//   //if statement evalautes false, the runs
+// }
+//
+//
+
+// Const magicNumber = 4
+// if (magicNumber === 1 ) {
+//   console.log(1);
+//
+//
+// } else if (magicNumber === 2) {
+//   console.log(2);
+// }else{ console.log('nomage number for you')
+// }
+
+
+// && (and also)
+// || (OR)
+
+// if (true || false) {
+//   console.log('success');
+// } else {
+//     console.log('no success');
+//
+// };
+//
+// const age = 80
+//
+// if(age === 21 || age === 80) {
+//   console.log('You are 21 or 80');
+// }
+//
+// const lastName = 'Rackas';
+//
+// if(lastName){ console.log('You did not provide a last name. ')}
+
+/**
+ prompt user for name
+ if they provide a name alert their name
+ if their provide no value alert tthat you need a vaid name
+ */
+
+
+// const userName = prompt('What is your name?');
+// If(userName);{
+//   alert(userName);
+// } else {
+//   alert('Invalid name');
+// }
+
+
+
+
+Const userAge =+ promp('what is your age?');
+if (userAge >= 18 && if(UserAge <= 80) {
+}
+const likesStarWars = confirm('do you like Star wars?');
+
+if(likesStarWars) {
+  alert('May the force be with you!');
+}
+else if (userAge > 80 ){
+  const likesPrunes = confirm ('do you like Prunes?')
+  likesPrunes ? alert('Gross');alert('I guess you are alright') 
+
+}else {
+alert('You are not old enought')
+}
