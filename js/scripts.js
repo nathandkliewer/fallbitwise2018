395ad930a49d9cdfb1c1c4cf03f0dfdfdee839cc
nathankliewer <nathandkliewer@gmail.com>
2018-11-14 19:13:06 -0800

    day3note

--- js/scripts.js
+++ js/scripts.js
@@ -1,116 +1,15 @@
-window.alert("Hello world!");
-alert("Hello world!");
-
-var myName = "Kliewer";
-
-    alert('Hello, ' + myName + '!'); //ES5
-
-    alert(`Hello, ${myName}!`); //ES6
-
-// const str1 = 'you are my brother Anika';
-// const str2 = new String(str1);
+// // always stot with 0 not 1
 //
-// console.log(str1 == str2);
-// console.log(str1 === str2);
-//
-// console.log(0.1+0.2)
-// console.log(.999999)
-// console.log(0/0);
+// const myFirstArry = {'Ben',29,true, 'Coalina, {'Norma,' 'Oliva', }'}
 //
 //
-// let number = 1;
-// console.log('number', number);
+// const geeksie = {'web for beginners', true},{'Mobile Friendly',true}.['Intro to JS',false}
 //
+// console.log(geekwise[1][0]); // -> Mobile Friendly
+// console.log(geekwise[0][0]); // -> Web for beginners
+// console.log(geekwise[2][1]); // -> false
+// console.log(geekwise[2]; // -> -> Mobile Friendly, true
 //
-// let cartTotal = 0;
+// console.console.log(geekwise.length); // few sec ago
 //
-// const item = 9.99;
-// const Item2 = 7.99;
-// const discount = 5.99;
-// on those
-///console.log(cartTotal += item);
-///console.log(cartTotal += Item2);
-///console.log(cartTotal -= discount);
-
-
-///let answer1 = prompt("Guess an number");
-///let answer2 = prompt("guess my number?");
-///alert(+answer1 + +answer2);
-
-///control flow
-//
-// const firstName = "Ben";
-// if (firstName === "Jacob"){
-//
-// console.log(Your name is Jacob);
-//
-//
-//
-//
-//   //if statment evluates true this run
-//   // Indent within curly braces
-//
-//   console.log('uzzah! This is true.')
-// }
-// else {
-//   //if statement evalautes false, the runs
-// }
-//
-//
-
-// Const magicNumber = 4
-// if (magicNumber === 1 ) {
-//   console.log(1);
-//
-//
-// } else if (magicNumber === 2) {
-//   console.log(2);
-// }else{ console.log('nomage number for you')
-// }
-
-
-// && (and also)
-// || (OR)
-
-// if (true || false) {
-//   console.log('success');
-// } else {
-//     console.log('no success');
-//
-// };
-//
-// const age = 80
-//
-// if(age === 21 || age === 80) {
-//   console.log('You are 21 or 80');
-// }
-//
-// const lastName = 'Rackas';
-//
-// if(lastName){ console.log('You did not provide a last name. ')}
-
-/**
- prompt user for name
- if they provide a name alert their name
- if their provide no value alert tthat you need a vaid name
- */
-
-// ask if user is over 18
-
-
-
-Const userAge =+ promp('what is your age?');
-if (userAge >= 18 && if(UserAge <= 80) {
-}
-const under18 = confirm('Sorry, you are not old enough');
-
-if(over) {
-  alert('May the force be with you!');
-}
-else if (userAge > 80 ){
-  const likesPrunes = confirm ('do you like Prunes?')
-  likesPrunes ? alert('Gross');alert('I guess you are alright')
-
-}else {
-alert('You are not old enought')
-}
+//  //
